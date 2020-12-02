2c4b93e3182907d8e726da5287a95d5f013472cd
Victoria Lee <victoriacmlee@gmail.com>
2020-12-02 15:12:17 -0500

    orm and burgers-orm setup

---
+++ config/orm.js
@@ -0,0 +1,23 @@
+const connection = require("./connection.js");
+
+const orm = {
+
+    // SELECT * FROM
+    all: function () {
+
+    },
+
+    // INTERT INTO
+    create: function () {
+
+    },
+
+    // UPDATE SET
+    update: function () {
+
+    },
+};
+
+
+// export orm
+module.exports = orm;
---
+++ model/burgers.js
@@ -0,0 +1,20 @@
+var orm = require("../config/orm.js");
+
+const burger = {
+
+    
+    all: function () {
+
+    },
+
+    create: function () {
+
+    },
+
+    update: function () {
+
+    }
+};
+
+// export burger orm
+module.exports = burger;
