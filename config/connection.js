75c43172820b711311b62b9e09dc49ecd6c675df
Victoria Lee <victoriacmlee@gmail.com>
2020-12-04 01:04:00 -0500

    update connection with JAWSDB implementation

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,24 @@
 var mysql = require("mysql");
 
-// setup mysql connection.
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "brag_a_burger_db"
-});
+if (process.env.JAWSDB_URL) {
+
+  connection = mysql.createConnection(process.envJAWS_URL)
+
+} else {
+
+  // setup mysql connection.
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "brag_a_burger_db"
+  });
+}
+
 
 // establish connection
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // export connection
 module.exports = connection;
